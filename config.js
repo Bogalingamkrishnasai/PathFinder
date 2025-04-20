dd08070e2a27e5a4c1b584b71a63c3322c842ca1
SaiNiranjanChitturi <147737575+SaiNiranjanChitturi@users.noreply.github.com>
2025-04-19 22:42:14 -0400

    .

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  cookieName: 'onmartSuperCenter',
-  port: 3025,
-  secret: 'bnr-secret-sauce', // in a real app this would be in a .env file/environment variable or other safe place
-};
